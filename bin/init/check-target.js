b7d056f89c747ef9799328b08ac19c5a94e01f9c
dustinpfister <dustin.pfister@gmail.com>
2019-10-19 12:42:26 -0400

    external check-target.js file

---
+++ bin/init/check-target.js
@@ -0,0 +1,34 @@
+// using the fs module, and the promisify method
+// of the util module
+let fs = require('fs'),
+promisify = require('util').promisify,
+
+// promisify fs.stat, and fs.readdir
+stat = promisify(fs.stat),
+readdir = promisify(fs.readdir);
+
+// the check target method is the only thing exported
+module.exports = (target) => {
+
+    // get stats
+    return stat(target)
+
+    // if the target is a dir, read the contents
+    .then((stat) => {
+        if (stat.isDirectory()) {
+            return readdir(target);
+        }
+        // if not a dir fail
+        return Promise.reject(new Error('target is not a directory.'));
+    })
+
+    // just resolve if dir contents are empty
+    .then((files) => {
+        if (files.length === 0) {
+            return Promise.resolve();
+        }
+        // fail if there are contents
+        return Promise.reject(new Error('The directory is not empty'));
+    });
+
+};
--- bin/init/index.js
+++ bin/init/index.js
@@ -1,31 +1,6 @@
 #!/usr/bin/env node
-let fs = require('fs'),
-promisify = require('util').promisify,
 
-stat = promisify(fs.stat),
-readdir = promisify(fs.readdir);
-
-// check if target is an empty dir
-let checkTarget = (target) => {
-    // get stats
-    return stat(target)
-    // if not dir fail, else readdir
-    .then((stat) => {
-        if (stat.isDirectory()) {
-            return readdir(target);
-        }
-        return Promise.reject(new Error('target is not a directory.'));
-    })
-
-    .then((files) => {
-
-        if (files.length === 0) {
-            return Promise.resolve();
-        }
-        return Promise.reject(new Error('The directory is not empty'));
-    })
-
-};
+let checkTarget = require('./check-target.js');
 
 checkTarget(process.argv[2] || process.cwd())
 
@@ -40,5 +15,3 @@ checkTarget(process.argv[2] || process.cwd())
     console.log(e.message);
 
 })
-
-//console.log('this is nc-init for now');
