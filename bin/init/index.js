1d5303304e6f5f97a41b055619a27236c060f546
dustinpfister <dustin.pfister@gmail.com>
2019-10-18 21:23:12 -0400

    target check works

--- bin/init/index.js
+++ bin/init/index.js
@@ -7,58 +7,38 @@ readdir = promisify(fs.readdir);
 
 // check if target is an empty dir
 let checkTarget = (target) => {
-
-    stat(target)
-
+    // get stats
+    return stat(target)
+    // if not dir fail, else readdir
     .then((stat) => {
-
         if (stat.isDirectory()) {
-
-            console.log(stat)
-
-        } else {
-
-            return Promise.reject(new Error('target is not a directory.'))
-
+            return readdir(target);
         }
-
+        return Promise.reject(new Error('target is not a directory.'));
     })
 
-    .catch((e) => {
+    .then((files) => {
 
-        console.log(e.message);
-
+        if (files.length === 0) {
+            return Promise.resolve();
+        }
+        return Promise.reject(new Error('The directory is not empty'));
     })
-
-    /*
-    return new Promise((resolve, reject) => {
-    fs.stat(target, (e, stat) => {
-    if (e) {
-    reject(e);
-    } else {
-    if (stat.isDirectory()) {
-    fs.readdir(target, (e, files) => {
-    if (e) {
-    reject(e)
-    } else {
-    if (files.length === 0) {
-    resolve()
-    } else {
-    reject(new Error('the target dir is not empty'));
-    }
-    }
-    });
-    } else {
-    reject(new Error('target is not a directory'));
-    }
-    }
-    });
-    });
-
-     */
 
 };
 
 checkTarget(process.argv[2] || process.cwd())
+
+.then(() => {
+
+    console.log('the folder is good');
+
+})
+
+.catch((e) => {
+
+    console.log(e.message);
+
+})
 
 //console.log('this is nc-init for now');
