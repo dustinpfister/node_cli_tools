a57631fbf365886f4e91faba1331eb937a8d1ccb
dustinpfister <dustin.pfister@gmail.com>
2019-10-22 20:51:38 -0400

    for_post

--- bin/ssg/lib/for_post.js
+++ bin/ssg/lib/for_post.js
@@ -3,21 +3,17 @@ promisify = require('util').promisify,
 readFile = promisify(fs.readFile),
 marked = require('marked');
 
+// is markdown helper
 let isMarkdown = (item) => {
-
     // is the item a markdown file?
     if (item.stat.isFile && item.fileName.match(/.md$/)) {
-
         return Promise.resolve()
-
     } else {
-
         return Promise.reject(new Error('item in posts folder is not a markdown file'));
-
     }
-
 }
 
+// main forFile method to be used with nc-walk
 module.exports = (item, next) => {
 
     isMarkdown(item)
