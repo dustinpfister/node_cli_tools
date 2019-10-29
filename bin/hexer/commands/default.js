9d574f61e43fe34647efbc97f5368cd8993a58e2
dustin pfister <dustin.pfister@gmail.com>
2019-10-29 17:41:50 -0400

    can convert text to rawhex format and back

--- bin/hexer/commands/default.js
+++ bin/hexer/commands/default.js
@@ -1,5 +1,9 @@
 // default command for nc-hexer
 let path = require('path'),
+fs = require('fs'),
+promisify = require('util').promisify,
+readFile = promisify(fs.readFile),
+writeFile = promisify(fs.writeFile),
 hexer = require('../hexer.js');
 
 exports.command = '*';
@@ -14,13 +18,57 @@ exports.builder = {
     // output file
     o: {
         default: path.join(process.cwd(), 'hex_'+ new Date().getTime() +'.txt')
+    },
+    // mode
+    m: {
+        default: 'toraw'
+    },
+    // return char
+    r: {
+        default: '\n'
     }
+    
 };
 exports.handler = function (argv) {
     console.log('nc-hexer default command:');
     
-    let raw = hexer.toRawStyle('foo\r\nbar\nfeeb');
+    if(!argv.t){
+        
+        console.log('a target file must be given:');
+        console.log('$ nc-hexer -t foo.txt');
+        
+    }else{
+        
+        readFile(argv.t, 'utf8')
+        
+        .then((data)=>{
+            
+            switch(argv.m){
+                
+                case 'toraw':
+                    return writeFile(argv.o, hexer.toRawStyle(data, argv.r));
+                break;
+                
+                case 'fromraw':
+                    return writeFile(argv.o, hexer.fromRawStyle(data, argv.r));
+                break;
+            }
+            
+            return Promise.reject(new Error('unknown mode'));
+            
+        })
+        
+        .catch((e)=>{
+            
+            console.warn(e.message);
+            
+        });
+        
+        
+    }
     
-    console.log(hexer.fromRawStyle(raw));
+    //let raw = hexer.toRawStyle('foo\r\nbar\nfeeb');
+    
+    //console.log(hexer.fromRawStyle(raw));
     
 };
