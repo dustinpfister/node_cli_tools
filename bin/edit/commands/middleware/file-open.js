b2960c9d47fdefa9a88f0718b4a57ea51b33d7e6
dustinpfister <dustin.pfister@gmail.com>
2019-10-29 11:57:54 -0400

    file-open middleware now handles erros better

--- bin/edit/commands/middleware/file-open.js
+++ bin/edit/commands/middleware/file-open.js
@@ -3,32 +3,41 @@ let express = require('express'),
 fs = require('fs'),
 path = require('path');
 
-
 module.exports = (conf) => {
 
     let router = express.Router();
 
-    router.use( require('body-parser').json() );
-    
-    router.post('/', (req, res)=>{
-        
-        fs.readFile( path.join(conf.target, req.body.fileName), 'utf8', (e, data) => {
-            
-            
-            res.json({
-            
-                mess: 'so far so good',
-                data: data,
-                fileName: req.body.fileName
-            
-            });
-            
-            
+    router.use(require('body-parser').json());
+
+    router.post('/', (req, res) => {
+
+        fs.readFile(path.join(conf.target, req.body.fileName), 'utf8', (e, text) => {
+
+            if (text) {
+                res.json({
+
+                    mess: 'looks like we have some text',
+                    text: text,
+                    fileName: req.body.fileName
+
+                });
+
+            } else {
+
+                res.json({
+
+                    mess: e.message,
+                    text: '',
+                    fileName: req.body.fileName
+
+                });
+
+            }
+
         });
-        
-        
+
     });
 
     return router;
-    
+
 };
--- bin/edit/commands/public/js/client.js
+++ bin/edit/commands/public/js/client.js
@@ -29,50 +29,17 @@ var vue = new Vue({
                 this.$http.post('/file-open', {
                     fileName: this.$data.fileName
                 })
-				.then(function(res){
-					
-					console.log(res)
-					
-				})
+                .then(function (res) {
+
+                    console.log(res);
+					if(res.body.data){
+						
+						this.$data.text = data;
+						
+					}
+
+                })
 
             }
         }
-    })
-
-    /*
-    var xml = new XMLHttpRequest();
-
-    xml.open('get', '/file-list');
-
-    xml.onreadystatechange = function(res){
-
-    if(xml.readyState === 4 && xml.status === 200){
-
-    console.log(JSON.parse(xml.response));
-
-    }
-
-
-    }
-
-    xml.send();
-     */
-
-    /*
-    var xml = new XMLHttpRequest();
-
-    xml.open('post', '/file-open');
-
-    xml.onreadystatechange = function(res){
-
-    if(xml.readyState === 4 && xml.status === 200){
-
-    console.log(JSON.parse(xml.response));
-
-    }
-
-
-    }
-    xml.setRequestHeader("Content-Type", "application/json");
-    xml.send(JSON.stringify({fileName:'first-post.md'}));
-    */
+    });
