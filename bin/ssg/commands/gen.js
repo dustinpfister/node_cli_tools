1899190cc65ec0a9bb19196ee47c09f704651baa
dustin pfister <dustin.pfister@gmail.com>
2019-10-31 12:24:20 -0400

    gen html of main index.html file

--- bin/ssg/commands/gen.js
+++ bin/ssg/commands/gen.js
@@ -8,20 +8,23 @@ exports.describe = 'generate command';
 exports.builder = {
     // root project path
     r: {
-    default:
-        process.cwd()
+        default:process.cwd()
     },
     // path to create the public folder in
     p: {
-    default:
-        path.join(process.cwd(), '_public')
+        default:path.join(process.cwd(), '_public')
+    },
+    // dir to folder of the theme to use
+    e:{
+        default: path.join(process.cwd(), '_themes/core')
     }
 };
 exports.handler = function (argv) {
 
     gen({
         dir_root: argv.r,
-        dir_public: argv.p
+        dir_public: argv.p,
+        dir_theme: argv.e
     });
 
 };
--- bin/ssg/lib/gen.js
+++ bin/ssg/lib/gen.js
@@ -1,6 +1,8 @@
-let exec = require('child_process').exec,
+//let exec = require('child_process').exec,
+let ejs = require('ejs'),
 promisify = require('util').promisify,
 mkdirp = promisify(require('mkdirp')),
+renderFile = promisify(ejs.renderFile),
 path = require('path'),
 walk = require('../../../shared/lib/walk/walk.js');
 
@@ -8,10 +10,8 @@ walk = require('../../../shared/lib/walk/walk.js');
 let genPosts = (opt) => {
     let path_script = path.resolve(__dirname, '../lib/for_post.js'),
     path_target = path.resolve(opt.dir_root, '_posts');
-
-    console.log('generating...');
-    
-    // walk
+    console.log('generating blog posts...');
+    // walk _posts
     walk.walk({
         dir: path_target,
         forFile: require(path_script),
@@ -20,23 +20,46 @@ let genPosts = (opt) => {
             dir_public: opt.dir_public
         }
     });
+};
 
-}
+let genIndex = (opt) => {
+    
+    console.log('building main index file uisng theme at:');
+    console.log(opt.dir_theme);
+    
+    let path_template_index = path.join(opt.dir_theme, 'index.ejs'),
+    ejs_locals = {
+        conf: opt,
+        title: 'site_foo main index'
+    },
+    ejs_options = {root: opt.dir_theme};
+    
+    renderFile( path_template_index, ejs_locals, ejs_options )
+    .then((html)=>{
+        
+        console.log(html);
+        
+    })
+    .catch((e)=>{
+        console.log('error building /index.html');
+        console.log(e.message);
+    });
+    
+};
 
 // exported method for gen.js
 module.exports = (opt) => {
-
     // make sure public folder is there
     mkdirp(opt.dir_public)
-
+    .then(()=>{
+        genIndex(opt);
+    })
     // gen posts
     .then(() => {
         genPosts(opt);
     })
-
     // if error
     .catch((e) => {
         console.log(e);
     });
-
 };
